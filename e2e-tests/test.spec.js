2c71c23d34471a5e3b05344f6138af1ea7b21151
Gabi Beteta <gabeque@gmail.com>
2024-04-23 09:35:58 +0200

    Add shell script

--- e2e-tests/test.spec.js
+++ e2e-tests/test.spec.js
@@ -7,4 +7,10 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('')
+    await page.click('text=ivysaur')
+    await expect(page.locator('text=chlorophyll')).toBeVisible()
+  })
 })
